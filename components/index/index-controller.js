22d754d43b97fed1d6b5c018b4a750383d1ed1a1
Martín Ferrarese <martin.ferrarese@grupoesfera.com.ar>
2021-10-23 20:55:56 -0300

    Routea para dar un saludo en el root

--- components/index/index-controller.js
+++ components/index/index-controller.js
@@ -5,10 +5,12 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
     try {
-        const mensaje = indexService.saludar();
+        const mensaje = await indexService.saludar();
         response.success(req, res, mensaje, 200);
     } catch (error) {
         console.log(error);
         response.failure(req, res, 'GET - No fue posible ejecutar el pedido', 500);
     }
-})
+});
+
+module.exports = router;
--- network/routes.js
+++ network/routes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const messageController = require('../components/message/message-controller');
+const indexController = require('../components/index/index-controller');
 
 const routes = (server) => {
+    server.use('/', indexController);
     server.use('/message', messageController);
 }
 
