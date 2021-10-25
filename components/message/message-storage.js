3f26c086226b42b4adf1e1c6266e8361d5f475c8
Martín Ferrarese <martin.ferrarese@grupoesfera.com.ar>
2021-10-24 21:18:10 -0300

    Persiste mensajes en la base de datos

--- components/message/message-storage.js
+++ components/message/message-storage.js
@@ -1,7 +1,18 @@
 const messageList = [];
+const db = require('mongoose');
+const Model = require('./message-model');
+
+const uri = 'mongodb://ferra31:<password>@comiditacluster0-shard-00-00.qcw0g.mongodb.net:27017,comiditacluster0-shard-00-01.qcw0g.mongodb.net:27017,comiditacluster0-shard-00-02.qcw0g.mongodb.net:27017/ComiditaCluster0?ssl=true&replicaSet=atlas-108pjv-shard-0&authSource=admin&retryWrites=true&w=majority';
+
+db.Promise = global.Promise;
+db.connect(uri, {
+    useNewUrlParser: true
+});
+console.log('Base de datos conectada correctamente');
 
 function addMessage(message) {
-    messageList.push(message);
+    const myMessage = new Model(message);
+    myMessage.save();
 }
 
 function getMessages() {
